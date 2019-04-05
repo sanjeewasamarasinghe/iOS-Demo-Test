a53680e69fedb64b5f560f1f56fc053ce4daec4e
sanjeewa samarasinghe <sanjeewasonline@hotmail.com>
2019-04-04 23:42:19 -0700

    add table view controller to container

---
+++ Iso-test-demo/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Iso-test-demo
+//
+//  Created by USER on 4/4/19.
+//  Copyright © 2019 USER. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var btnNext: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnNext(_ sender: Any) {
+       self.performSegue(withIdentifier: "mainNext", sender: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
