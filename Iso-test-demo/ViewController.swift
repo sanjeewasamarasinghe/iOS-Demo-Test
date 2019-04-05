417fbfe800dfe274e5ff252e9cd29866b0709427
sanjeewa samarasinghe <sanjeewasonline@hotmail.com>
2019-04-04 22:19:56 -0700

    add new project foe git hub

---
+++ Iso-test-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Iso-test-demo
+//
+//  Created by USER on 4/4/19.
+//  Copyright © 2019 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Iso-test-demoTests/Iso_test_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Iso_test_demoTests.swift
+//  Iso-test-demoTests
+//
+//  Created by USER on 4/4/19.
+//  Copyright © 2019 USER. All rights reserved.
+//
+
+import XCTest
+@testable import Iso_test_demo
+
+class Iso_test_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
