77c52dacb854436dfac498ac3845f36c262fbf72
sanjeewa samarasinghe <sanjeewasonline@hotmail.com>
2019-04-17 02:47:30 -0700

    Add Class AppTempData

---
+++ Iso-test-demo/AppTempData.swift
@@ -0,0 +1,16 @@
+//
+//  AppTempData.swift
+//  Iso-test-demo
+//
+//  Created by USER on 4/17/19.
+//  Copyright © 2019 USER. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class AppTempData {
+    static var userHandleApp: AuthStateDidChangeListenerHandle?
+}
